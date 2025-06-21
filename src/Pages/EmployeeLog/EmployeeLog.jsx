256f778cca1f3bd0d92f9d9822155e4b152d12b4
Rocky islam <mohammadrockyislam01@gmail.com>
2025-06-21 11:09:29 +0600

    dynamic done

--- src/Pages/EmployeeLog/EmployeeLog.jsx
+++ src/Pages/EmployeeLog/EmployeeLog.jsx
@@ -20,8 +20,42 @@ const EmployeeLog = () => {
           </div>
         </div>
         <div className="gap-5 flex">
-          <button className="btn bg-white px-7 py-2 font-semibold">Export Excel <span><RxDownload size={16}/></span></button>
-          <button className="btn btn-primary font-semibold"><span><FaUserPlus size={16}/></span> Add Employee</button>
+          <button className="btn bg-white px-7 py-2 font-semibold">
+            Export Excel{" "}
+            <span>
+              <RxDownload size={16} />
+            </span>
+          </button>
+          {/* add modal */}
+          <button
+            className="btn btn-primary font-semibold"
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+          >
+            <span>
+              <FaUserPlus size={16} />
+            </span>{" "}
+            Add Employee
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
       </div>
       <div className="mt-6">
--- src/component/Table/Table.jsx
+++ src/component/Table/Table.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { FaRegCircleCheck } from "react-icons/fa6";
 
 const Table = () => {
   const [employee, setEmployee] = useState([]);
   console.log(employee);
   const [searchTerm, setSearchTerm] = useState("");
 
-
   useEffect(() => {
     fetch("employeeData.json")
       .then((res) => res.json())
       .then((data) => {
         setEmployee(data);
-        
       })
       .catch((e) => console.log("error :", e));
   }, []);
@@ -22,6 +21,10 @@ const Table = () => {
       emp.employeeName.toLowerCase().includes(searchTerm.toLowerCase()) ||
       emp.id.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  // color class
+   
+
 
   return (
     <div className="bg-white px-4">
@@ -45,8 +48,8 @@ const Table = () => {
                 />
               </svg>
               <input
-              value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 type="text"
                 className="grow"
                 placeholder="Search by ID, Name"
@@ -63,12 +66,19 @@ const Table = () => {
             </select>
           </div>
           <div>
-            <select className="select select-bordered w-full max-w-xs">
+            <select className="select w-52">
               <option disabled selected>
-                Who shot first?
+                Status
               </option>
-              <option>Han Solo</option>
-              <option>Greedo</option>
+              <option className="text-[#089624] flex">
+                {" "}
+                <span>
+                  <FaRegCircleCheck size={20} /> Approved
+                </span>
+              </option>
+              <option className="text-red-500">
+                <FaRegCircleCheck /> Reject
+              </option>
             </select>
           </div>
           <div>
@@ -86,15 +96,16 @@ const Table = () => {
         <table className="table table-md rounded-md">
           <thead className="bg-[#F3F5F6]">
             <tr>
-              <th>ID</th>
-              <th>Employee Name</th>
-              <th>Duration</th>
-              <th>Start Time - End Time</th>
-              <th>Due Hours</th>
-              <th>Department</th>
-              <th>Project</th>
-              <th>Notes</th>
-              <th>Action</th>
+              <th className="text-base font-bold">ID</th>
+              <th className="text-base font-bold">Employee Name</th>
+              <th className="text-base font-bold">Duration</th>
+              <th className="text-base font-bold">Start Time - End Time</th>
+              <th className="text-base font-bold">Due Hours</th>
+              <th className="text-base font-bold">Department</th>
+              <th className="text-base font-bold">Project</th>
+              <th className="text-base font-bold">Notes</th>
+              <th className="text-base font-bold">Action</th>
+              <th className="text-base font-bold"></th>
             </tr>
           </thead>
           <tbody>
@@ -104,14 +115,22 @@ const Table = () => {
                   {"#"}
                   {emp.id}
                 </td>
-                <td>{emp.employeeName}</td>
-                <td>{emp.duration}</td>
-                <td>{emp.startTimeEndTime}</td>
-                <td>{emp.dueHours}</td>
-                <td>{emp.department}</td>
-                <td>{emp.project}</td>
-                <td>{emp.notes}</td>
-                <td>Action Btn</td>
+                <td className="text-base font-medium">{emp.employeeName}</td>
+                <td className="text-base font-medium">{emp.duration}</td>
+                <td className="text-base font-medium">{emp.startTimeEndTime}</td>
+                <td className="text-base font-medium">{emp.dueHours}</td>
+                <td className={`rounded-full ${emp.department == 'Development' ? "text-[#16A34A] bg-[#a8fec7]" : emp.department == "HR" ? " text-purple-300" : 'text-slate-800'}`} >{emp.department}</td>
+                <td className="text-base font-medium">{emp.project}</td>
+                <td className="text-base font-medium">{emp.notes}</td>
+                <td className="text-base font-medium">
+                  <div className="flex gap-2">
+                    <button className="text-red-400 ">Reject</button>
+                    <button className="btn text-white bg-[#16A34A]">approve</button>
+                  </div>
+                </td>
+                <td>
+                  <button>...</button>
+                </td>
               </tr>
             ))}
           </tbody>
